ac8b5d29c1464e09cfe8815d6eee0d7e81621dd8
vishal211294 <86513579+vishal211294@users.noreply.github.com>
2022-09-06 21:59:12 +0530

    Add files via upload

---
+++ PoliticsGateway/src/main/java/com/ey/politics/gateway/PoliticsGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ey.politics.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliticsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliticsGatewayApplication.class, args);
+	}
+
+}
